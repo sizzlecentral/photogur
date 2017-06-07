764ec4b94611423387b44d811155b726d196b101
Don Cooper <don_cooper@hotmail.com>
2017-06-07 14:37:48 -0400

    Exercise done 1st time

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
+
   root 'pictures#index'
-
-  get 'pictures' => 'pictures#index'
-  get 'pictures/new' => 'pictures#new'
-  get 'pictures/:id/edit' => "pictures#edit", as: 'edit_picture'
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  post 'pictures' => 'pictures#create'
-  patch 'pictures/:id' => "pictures#update"
-  delete 'pictures/:id' => 'pictures#destroy'
+  resources :pictures
 
 end
