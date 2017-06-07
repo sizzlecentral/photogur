86605338d375403e3b1e2f80391dc2ba5fb19672
Don Cooper <don_cooper@hotmail.com>
2017-06-07 14:32:41 -0400

    Edit working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-
   root 'pictures#index'
+
   get 'pictures' => 'pictures#index'
+  get 'pictures/new' => 'pictures#new'
+  get 'pictures/:id/edit' => "pictures#edit", as: 'edit_picture'
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
 
   post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
+  patch 'pictures/:id' => "pictures#update"
 
-  get 'pictures/:id' => 'pictures#show'
+
 
 end
