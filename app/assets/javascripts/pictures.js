bd6fbc4f395fb87b6d5957babbfe819f4e025d78
Don Cooper <don_cooper@hotmail.com>
2017-07-16 09:23:49 -0400

    Card counter working

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   var addToDeck = document.querySelectorAll('.active-state-trigger');
+  var deckCount = document.querySelector('.deck-count');
+  var count = 0;
+
   addToDeck.forEach(function(button){
     button.addEventListener('click', function(event) {
       event.preventDefault();
       var parentCard = button.parentElement;
       if (parentCard.classList.contains('active')) {
         parentCard.classList.remove('active');
+        count--;
       } else {
         parentCard.classList.add('active');
+        count++;
       }
+      deckCount.innerHTML = count;
     });
   });
+
 });
