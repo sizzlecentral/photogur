62b35a344a096f79a2135509124a446706ed71d6
Don Cooper <don_cooper@hotmail.com>
2017-07-16 09:01:33 -0400

    Active class added to cards when button is clicked

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,3 +1,11 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
+document.addEventListener("DOMContentLoaded", function () {
+
+  var addToDeck = document.querySelectorAll('.active-state-trigger');
+  addToDeck.forEach(function(button){
+    button.addEventListener('click', function(event) {
+      event.preventDefault();
+      var parentCard = button.parentElement;
+      parentCard.classList.add('active');
+    });
+  });
+});
