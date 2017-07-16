0506d1cb16d458a7ed7e03f52903939fa796c40c
Don Cooper <don_cooper@hotmail.com>
2017-07-16 09:54:26 -0400

    Cards adding and removing from current deck, exercise complete

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   var addToDeck = document.querySelectorAll('.active-state-trigger');
+  var currentDeck = document.querySelector('.current-deck')
   var deckCount = document.querySelector('.deck-count');
   var count = 0;
 
@@ -11,10 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
       if (parentCard.classList.contains('active')) {
         parentCard.classList.remove('active');
         button.innerText = 'Add to deck';
+        currentDeck.removeChild(parentCard);
         count--;
       } else if (count < 5) {
         parentCard.classList.add('active');
         button.innerText = 'Remove from deck';
+        currentDeck.appendChild(parentCard);
         count++;
       } else {
         alert('You have reached your maximum number of cards.  Please remove one of your cards from the deck to add a new one.')
